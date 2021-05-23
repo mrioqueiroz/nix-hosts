b011ea9bd386d58676a8d8e8f1d98fbb39632b94
Mário Queiroz <mrioqueiroz@protonmail.com>
2021-05-23 13:44:50 -0300

    Enable monitoring and intrusion detection

--- deployment.nix
+++ deployment.nix
@@ -49,10 +49,80 @@ in {
         passwordAuthentication = false;
       };
 
+      sshguard = {
+        enable = true;
+        blocktime = 900;
+        attack_threshold = 5;
+        # This will block the IP definitely.
+        blacklist_threshold = 10;
+      };
+
+      fail2ban = {
+        enable = true;
+      };
+
+      prometheus = {
+        enable = true;
+        exporters = {
+          node = {
+            enable = true;
+            enabledCollectors = [ "systemd" ];
+          };
+        };
+        scrapeConfigs = [
+          {
+            job_name = "prometheus";
+            static_configs = [
+              {
+                targets = [ "localhost:9090" ];
+              }
+            ];
+          }
+          {
+            job_name = "node";
+            static_configs = [
+              {
+                targets = [ "localhost:9100" ];
+              }
+            ];
+          }
+        ];
+      };
+
+      # After deployed, add the dashboards 1860 abd 405 and you are good to go.
+      grafana = {
+        enable = true;
+        # Despite having defined the credentials here, I still had to set admin
+        # password when accessing Grafana the first time.
+        # security = {
+          # Security warning: Grafana passwords will be stored as plain text in
+          # the Nix store.
+          # adminUser = "admin";
+          # adminPassword = "somerandompassword";
+        # };
+        provision = {
+          enable = true;
+          # Needs to be type `list of submodules'. See more about datasources
+          # in https://grafana.com/docs/grafana/latest/datasources/prometheus/
+          datasources = [
+            {
+              name = "Prometheus";
+              type = "prometheus";
+              url = "http://localhost:9090";
+              # Use direct when accessing Prometheus from the browser.
+              # Use proxy when accessing through Grafana.
+              access = "proxy";
+            }
+          ];
+        };
+      };
+
       nginx = {
         enable = true;
-        virtualHosts."mrioqueiroz.com" = {
-          locations."/".root = (
+        virtualHosts = {
+          "mrioqueiroz.com" = {
+            default = true;
+            locations."/".root = (
             # Build the website with Zola and make it available to Nginx. It
             # would also be possible to put it on a different file (say
             # ./default.nix) and import this derivation with (import ./.).
@@ -67,9 +137,17 @@ in {
             # make sure the port 443 is open and, consequently, avoid the 521
             # error.
             addSSL = true;
+            # Needs to define this option to avoid "option sslCertificate used
+            # but not defined" error when addSSL is set to true.
+            enableACME = true;
+          };
+          "grafana.mrioqueiroz.com" = {
+            locations."/".proxyPass = "http://localhost:3000";
+            addSSL = true;
             enableACME = true;
           };
         };
       };
     };
-  }
+  };
+}
